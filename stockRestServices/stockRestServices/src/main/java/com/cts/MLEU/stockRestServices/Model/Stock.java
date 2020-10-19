ab01698670dc74650840aabe7cd95bc4a5683aa8
vilascognizantnew <vilascognizant@123>
2020-10-19 14:30:56 +0530

    Model class commit

---
+++ stockRestServices/stockRestServices/src/main/java/com/cts/MLEU/stockRestServices/Model/Stock.java
@@ -0,0 +1,62 @@
+package com.cts.MLEU.stockRestServices.Model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Stock {
+	
+	@Id
+	private int stock_id;
+	private String stock_name;
+	private int price;
+	private int volume;
+	private Date date;
+	public int getStock_id() {
+		return stock_id;
+	}
+	public void setStock_id(int stock_id) {
+		this.stock_id = stock_id;
+	}
+	public String getStock_name() {
+		return stock_name;
+	}
+	public void setStock_name(String stock_name) {
+		this.stock_name = stock_name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getVolume() {
+		return volume;
+	}
+	public void setVolume(int volume) {
+		this.volume = volume;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public Stock() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Stock(int stock_id, String stock_name, int price, int volume, Date date) {
+		super();
+		this.stock_id = stock_id;
+		this.stock_name = stock_name;
+		this.price = price;
+		this.volume = volume;
+		this.date = date;
+	}
+	
+	
+
+}
