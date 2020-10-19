ccddb17eec6338ff0a3187037f90bc0f09f77931
vilascognizantnew <vilascognizant@123>
2020-10-19 16:07:39 +0530

    security commit

---
+++ stockRestServices/stockRestServices/src/main/java/com/cts/MLEU/stockRestServices/Config/RestSecurityConfig.java
@@ -0,0 +1,40 @@
+package com.cts.MLEU.stockRestServices.Config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class RestSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http
+		.csrf()
+		.disable()
+		.authorizeRequests()
+		.antMatchers(HttpMethod.GET,"/stock/**").hasAnyRole("USER")
+		.antMatchers(HttpMethod.POST,"/stock/**").hasAnyRole("ADMIN")
+		.anyRequest()
+		.authenticated()
+		.and()
+		.httpBasic();
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("user")
+			.password("{noop}user")
+			.roles("USER")
+			.and()
+			.withUser("admin")
+			.password("{noop}admin")
+			.roles("ADMIN");
+	}
+
+}
--- stockRestServices/stockRestServices/src/main/java/com/cts/MLEU/stockRestServices/Controller/StockController.java
+++ stockRestServices/stockRestServices/src/main/java/com/cts/MLEU/stockRestServices/Controller/StockController.java
@@ -11,27 +11,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.cts.MLEU.stockRestServices.Model.Stock;
 import com.cts.MLEU.stockRestServices.Service.StockService;
 
-@Controller
+@RestController
+@RequestMapping(value="/stock/")
 public class StockController {
 
 	@Autowired
 	StockService stockService;
 	
-	@GetMapping("/stocks")
+	@GetMapping("/getStocks")
 	public ResponseEntity<Object> findAll() {
 		return new ResponseEntity<>(stockService.getAllStock(), HttpStatus.OK);
 	}
 	
-	@GetMapping("/stocks/{id}")
+	@GetMapping("/getStocksById/{id}")
 	public ResponseEntity<Object> getById(@PathVariable int id ){
 		return new ResponseEntity<>(stockService.getStockById(id),HttpStatus.OK); 
 	}
 	
-	@PostMapping("/stocks")
+	@PostMapping("/addStock")
 	public ResponseEntity<Object> addEmployee(@RequestBody Stock stock){
 		return new ResponseEntity<>(stockService.addStock(stock),HttpStatus.OK);
 	}
