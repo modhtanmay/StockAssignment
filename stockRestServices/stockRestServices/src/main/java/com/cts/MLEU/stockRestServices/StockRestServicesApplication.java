431fb54fd2307a267cb471bb4180afa1ec819fab
vilascognizantnew <vilascognizant@123>
2020-10-19 14:23:08 +0530

    initial commit

---
+++ stockRestServices/stockRestServices/src/main/java/com/cts/MLEU/stockRestServices/StockRestServicesApplication.java
@@ -0,0 +1,13 @@
+package com.cts.MLEU.stockRestServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockRestServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockRestServicesApplication.class, args);
+	}
+
+}
---
+++ stockRestServices/stockRestServices/src/test/java/com/cts/MLEU/stockRestServices/StockRestServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.MLEU.stockRestServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockRestServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
